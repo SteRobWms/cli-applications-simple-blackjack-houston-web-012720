0deb4d6fa2f6a063a1130d80f117f78353682dc8
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-25 04:06:27 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,17 +35,17 @@ end
 
 def hit?(n)
   prompt_user
-  answer = get_user_input
-  if get_user_input = "s"
-    display_card_total(n)
-  elsif get_user_input = "h"
-    card_total += deal_card
-    display_card_total(n)
-  else
-    invalid_command
-    prompt_user
-  end
-  display_card_total(n)
+  get_user_input
+  # if get_user_input = "s"
+  #   display_card_total(n)
+  # elsif get_user_input = "h"
+  #   n += 
+  #   display_card_total(n)
+  # else
+  #   invalid_command
+  #   prompt_user
+  # end
+  # display_card_total(n)
 end
 
 def invalid_command
